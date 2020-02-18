b5b918ed2f7f5c8ec1be22cede71fb7955ad3e5a
Santosh Kumar <sntshkmr60@gmail.com>
2020-02-19 04:14:10 +0530

    sys.exit() if there is no entry on --list

--- diariolib/formatter.py
+++ diariolib/formatter.py
@@ -1,7 +1,10 @@
+import sys
 import json
 
 def json_to_row(json_data: dict) -> str:
     list_of_entries = json.loads(json_data)
+    if list_of_entries is None:
+        sys.exit("There is no diary entry to list. Start writing one.")
     template = "{entry_id} {datetime} {text}"
     for entry in list_of_entries:
         print(template.format(entry_id=entry["id"],
