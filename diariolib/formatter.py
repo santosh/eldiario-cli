7b0597ed9e1d6d66cfd732bc4c5be8d312a52946
Santosh Kumar <sntshkmr60@gmail.com>
2020-02-19 05:27:43 +0530

    Beautify --list output

--- diariolib/formatter.py
+++ diariolib/formatter.py
@@ -1,12 +1,22 @@
 import sys
 import json
 
+UUID_THRESHOLD = 8
+DATETIME_THRESHOLD = 10
+TEXT_THRESHOLD = 30
+
 def json_to_row(json_data: dict) -> str:
     list_of_entries = json.loads(json_data)
     if list_of_entries is None:
         sys.exit("There is no diary entry to list. Start writing one.")
+
     template = "{entry_id} {datetime} {text}"
+    # Very dirty, needs cleanup
     for entry in list_of_entries:
-        print(template.format(entry_id=entry["id"],
-                              datetime=entry["datetime"],
-                              text=entry["body"]))
+        print(
+            template.format(
+                entry_id=entry["id"][:UUID_THRESHOLD],
+                datetime=entry["datetime"][:DATETIME_THRESHOLD],
+                text=entry["body"][:TEXT_THRESHOLD] + "..."
+                )
+            )
